6dd926a444c34c42db523a2a0c55e130f51b6427
Saleh-AlSaeed <salreggea1@gmail.com>
2025-09-20 21:15:53 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,12 @@ const __dirname = path.dirname(__filename);
 
 // انتبه: مجلد public موجود بجذر المشروع (وليس داخل src)
 const ROOT_DIR = path.join(__dirname, '..');
-
+app.get('/vendor/livekit-client.umd.min.js', (req, res, next) => {
+  res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  next();
+});
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
